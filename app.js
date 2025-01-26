85f0dbf6f2cd54274ddd7084bf7bc8f9bd7990ec
Phi Duc <phivanduc325@gmail.com>
2025-01-27 06:23:55 +0700

    Last

--- app.js
+++ app.js
@@ -12,7 +12,6 @@ const indexRouter = require('./routes/index');
 const app = express();
 
 // view engine setup
-app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
@@ -24,9 +23,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors(corsConfig));
 
-app.use('/', (req, res) => {
-  res.send("<h1>ExpressJS<h1/>")
-})
 app.use('/api', indexRouter);
 
 // catch 404 and forward to error handler
