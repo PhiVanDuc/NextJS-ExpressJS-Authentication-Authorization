9730a23680b656b241a9c2a507f950b1689d1c1e
Phi Duc <phivanduc325@gmail.com>
2025-01-27 05:07:47 +0700

    .

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ var router = express.Router();
 
 const tokenRouter = require("./token");
 
+router.get("/", () => {
+    res.send("Welcome to the ExpressJS!");
+});
 router.use(tokenRouter);
 
 module.exports = router;
