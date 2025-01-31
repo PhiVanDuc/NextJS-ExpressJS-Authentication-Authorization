2085e03aae8f9e15632dcf1f677b24d9a30b3dff
Phi Duc <phivanduc325@gmail.com>
2025-01-31 23:47:33 +0700

    Update protected api

--- controllers/blog.controller.js
+++ controllers/blog.controller.js
@@ -17,5 +17,85 @@ module.exports = {
                 message: error.message,
             })
         }
-    }
+    },
+
+    getBlogs2: async (req, res) => {
+        try {
+            const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_per_page=10");
+            const posts = await postsResponse.json();
+
+            return res.status(200).json({
+                success: true,
+                message: "Thàng công lấy ra postes (protected)",
+                data: { posts },
+            });
+        }
+        catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+            })
+        }
+    },
+
+    getBlogs3: async (req, res) => {
+        try {
+            const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_per_page=10");
+            const posts = await postsResponse.json();
+
+            return res.status(200).json({
+                success: true,
+                message: "Thàng công lấy ra postes (protected)",
+                data: { posts },
+            });
+        }
+        catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+            })
+        }
+    },
+
+    getBlogs4: async (req, res) => {
+        try {
+            const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_per_page=10");
+            const posts = await postsResponse.json();
+
+            return res.status(200).json({
+                success: true,
+                message: "Thàng công lấy ra postes (protected)",
+                data: { posts },
+            });
+        }
+        catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+            })
+        }
+    },
+
+    getBlogs5: async (req, res) => {
+        try {
+            const postsResponse = await fetch("https://jsonplaceholder.typicode.com/posts?_page=1&_per_page=10");
+            const posts = await postsResponse.json();
+
+            return res.status(200).json({
+                success: true,
+                message: "Thàng công lấy ra postes (protected)",
+                data: { posts },
+            });
+        }
+        catch(error) {
+            console.log(error);
+            return res.status(500).json({
+                success: false,
+                message: error.message,
+            })
+        }
+    },
 }
--- routes/blog.js
+++ routes/blog.js
@@ -4,5 +4,9 @@ const router = express.Router();
 const blogController = require("../controllers/blog.controller");
 
 router.get("/blogs", blogController.getBlogs);
+router.get("/blogs2", blogController.getBlogs2);
+router.get("/blogs3", blogController.getBlogs3);
+router.get("/blogs4", blogController.getBlogs4);
+router.get("/blogs5", blogController.getBlogs5);
 
 module.exports = router;
